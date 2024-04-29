7b0102e4424d836b533c4cb48acde709bbe96e99
muhamedsafwat <musafwat666@gmail.com>
2024-04-29 14:52:25 +0300

    initial file upload

---
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Button button = new Button("Hello World");
+        Scene scene = new Scene(button, 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
